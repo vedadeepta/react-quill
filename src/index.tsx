1a83d8737548fa5bc9d46ddfe1c22760603d8aa4
vedadeepta <pal.veda.deepta@gmail.com>
2021-08-16 18:33:15 +0530

    fix: update convert api

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-/*
+  /*
 React-Quill
 https://github.com/zenoamaro/react-quill
 */
@@ -382,7 +382,7 @@ class ReactQuill extends React.Component<ReactQuillProps, ReactQuillState> {
     this.value = value;
     const sel = this.getEditorSelection();
     if (typeof value === 'string') {
-      editor.setContents(editor.clipboard.convert(value));
+      editor.setContents(editor.clipboard.convert({ html: value }));
     } else {
       editor.setContents(value);
     }
